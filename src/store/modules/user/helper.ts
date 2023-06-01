943a6486782a98afcc5240bd9a6e5b148254481a
jiucheng-kj <69421775+jiucheng-kj@users.noreply.github.com>
2023-06-01 22:00:23 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      name: '久橙',
+      description: 'by <a href="https://www.518tv.cn" class="text-blue-500" target="_blank" >久橙</a>',
     },
   }
 }
